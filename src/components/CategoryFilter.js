e3006596c0a00f70adb22eda8a9132818dee6039
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-10 13:44:15 +0300

    Implemented App .js component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 function CategoryFilter({categories, selectedCategory, onSelectCategory}) {
-  // mapping our category data 
   const categoryBtn = categories.map((category)=>{
-    const className = category == selectedCategory ? "selected" : null;
-    return ( 
-      <button
-      key={category}
-      className = {className}
-      onClick = {()=> onSelectCategory(category)}>
+    const className = category === selectedCategory ? "selected" : null;
+    return(
+      <button key={category}
+        className={className}
+        onClick={()=>onSelectCategory(category)}
+      >
         {category}
       </button>
     )
